51c1389f0d6400588de68d722c7d739f23c93c9c
Jay Patel <jpatel@shutterstock.com>
2016-06-08 19:54:00 -0400

    add message on connect/disconnect

--- server.js
+++ server.js
@@ -9,13 +9,19 @@ var server = http.Server(app);
 var io = socket_io(server);
 
 io.on('connection', (socket) => {
-  console.log('Client connected');
-
+  console.log('Client connected', socket.server.engine.clientsCount);
+  socket.broadcast.emit('message', `User Joined! ${socket.id}`);
+  
   socket.on('message', (message) => {
     console.log('Received message:', message);
     socket.broadcast.emit('message', message);
   });
+  socket.on('disconnect', (message) => {
+    console.log('This guy peaced out.:', socket.id);
+    socket.broadcast.emit('message', `User Left! ${socket.id}`);
+  });
 });
+
 
 
 server.listen(8080);
