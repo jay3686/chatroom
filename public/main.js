d8936628b029d569e6b58c2b917390ce9fb9a1bd
Jay <jay3686@gmail.com>
2016-06-30 23:48:53 +0000

    incremental commit

--- public/main.js
+++ public/main.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
   var messages = $('#messages');
   var typingMessage = $('#typingMessage');
   var totalCount = $('#totalCount');
+  var userList = $('#userList');
 
   var addMessage = function (message) {
     messages.append('<div>' + message + '</div>');
@@ -25,22 +26,21 @@ $(document).ready(function () {
     }
   };
 
-  var updateConnectedCount = function (numUsers) {
-    totalCount.text(numUsers);
+  var updateConnected = function (users) {
+    totalCount.text(users.length);
+    userList.html(users.map((k) => `<li>${k}</li>`).join(''));
   };
 
   var typing = false;
   var timer;
 
   input.on('keydown', function (event) {
-    socket.emit('isTyping', true);
     if(!typing) {
-      console.log('user is typing');      
+      socket.emit('isTyping', true);
     }
     typing = true;
     clearTimeout(timer);
     timer = setTimeout(function (event) {
-      console.log('user stopped typing');
       typing = false;
       socket.emit('isTyping', false);
     }, 3000);
@@ -57,6 +57,6 @@ $(document).ready(function () {
 
   socket.on('message', addMessage);
   socket.on('isTyping', updateIsTyping);
-  socket.on('totalCount', updateConnectedCount);
+  socket.on('totalUsers', updateConnected);
 
 });
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var socket_io = require('socket.io');
 var http = require('http');
 var express = require('express');
+var ObservableSet = require('observable-set');
 
 var app = express();
 app.use(express.static('public'));
@@ -11,8 +12,9 @@ var totalCount = 0;
 var server = http.Server(app);
 var io = socket_io(server);
 
-var users = {};
-var typers = {};
+var userNames = {};
+var usersIds = new ObservableSet([]);
+var typers = new ObservableSet([]);
 
 function usernameTaken(username) {
   return Object.keys(users).some((key) => users[key].username === username);
@@ -29,7 +31,7 @@ function handleLogin(socket, username) {
     socket.emit('message', `'${users[socket.id].username}' changed their name to '${username}'.`);
     socket.broadcast.emit('message', `'${users[socket.id].username}' changed their name to '${username}'.`);
 
-    users[socket.id] = { username };
+    usersNames[socket.id] = { username };
     console.log('added user', username, users);
   }
 }
@@ -49,18 +51,19 @@ function handleCommand(socket, message) {
 }
 
 function assignUsername(socket) {
-  users[socket.id] = { username: socket.id };
+  userNames[socket.id] = socket.id;
 }
 
 io.on('connection', (socket) => {
   assignUsername(socket);
-  console.log('Client connected', users[socket.id].username);
+  console.log('Client connected', userNames[socket.id]);
 
-  socket.broadcast.emit('message', `User Joined! ${users[socket.id].username}`);
-  socket.emit('message', `User Joined! ${users[socket.id].username}`);
+  socket.broadcast.emit('message', `User Joined! ${userNames[socket.id]}`);
+  socket.emit('message', `User Joined! ${userNames[socket.id]}`);
 
-  socket.broadcast.emit('totalCount', ++totalCount);
-  socket.emit('totalCount', totalCount);
+  const typingUsers = Object.keys(users).map((key) => (userNamess[key]));
+  socket.broadcast.emit('totalUsers', typingUsers);
+  socket.emit('totalUsers', typingUsers);
 
   socket.on('message', (message) => {
     console.log('Received message:', message);
@@ -68,14 +71,15 @@ io.on('connection', (socket) => {
       handleCommand(socket, message);
     } else {
       // TODO: let the frontend decide how to style this maybe
-      socket.broadcast.emit('message', `${users[socket.id].username}: ${message}`);
+      socket.broadcast.emit('message', `${userNames[socket.id]}: ${message}`);
     }
   });
   
   socket.on('disconnect', () => {
-    console.log('This guy peaced out.:', users[socket.id].username);
-    socket.broadcast.emit('message', `User Left! ${users[socket.id].username}`);
-    socket.broadcast.emit('totalCount', --totalCount);
+    console.log('This guy peaced out.:', userNames[socket.id]);
+    socket.broadcast.emit('message', `User Left! ${userNames[socket.id]}`);
+    const typingUsers = Object.keys(users).map((key) => (userNames[key]));
+    socket.broadcast.emit('totalUsers', typingUsers);
     // TODO: rethink below if we have persistent users
     delete users[socket.id];
     delete typers[socket.id];
@@ -88,7 +92,7 @@ io.on('connection', (socket) => {
       typers[socket.id] = true;
     }
 
-    const typingUsers = Object.keys(typers).map((key) => (users[key].username));
+    const typingUsers = Object.keys(typers).map((key) => (userNames[key]));
     console.log('Is typing:', typingUsers);
     socket.broadcast.emit('isTyping', typingUsers);
   });
